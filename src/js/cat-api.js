ec6f215d93a8f5ea58a7ab11e3bd882c2d567b52
Tamira2804 <tamira2804@gmail.com>
2023-06-27 21:40:15 +0200

    commit

---
+++ src/js/cat-api.js
@@ -0,0 +1,43 @@
+const API_KEY =
+  'live_TrS19Kwl97618lIqTZGGNvqd8Kmru4aLtkXcdSHBDvu9xpJN346l82g7ukXxKBhf';
+const BASE_URL = `https://api.thecatapi.com/v1`;
+
+refs = {
+  breedSelect: document.querySelector('.breed-select'),
+  loader: document.querySelector('.loader'),
+};
+
+export function fetchBreeds() {
+  refs.breedSelect.classList.add('hidden');
+  refs.loader.classList.remove('hidden');
+
+  return fetch(`${BASE_URL}/breeds?api_key=${API_KEY}`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+      return response.json();
+    })
+    .catch(error => {
+      console.error(error);
+    });
+}
+
+export function fetchCatByBreed(breedId) {
+  return fetch(
+    `${BASE_URL}/images/search?breed_ids=${breedId}&api_key=${API_KEY}`
+  )
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Failed to fetch cat by breed');
+      }
+      return response.json();
+    })
+    .then(cat => {
+      return cat[0];
+    })
+    .catch(error => {
+      console.error('Error fetching cat data:', error);
+      throw error;
+    });
+}
---
+++ src/js/index.js
@@ -0,0 +1,66 @@
+import { fetchBreeds, fetchCatByBreed } from './cat-api.js';
+import '../css/style.css';
+import SlimSelect from 'slim-select';
+import '/node_modules/slim-select/dist/slimselect.css';
+import Notiflix from 'notiflix';
+
+refs = {
+  breedSelect: document.querySelector('.breed-select'),
+  loader: document.querySelector('.loader'),
+  error: document.querySelector('.error'),
+  catInfo: document.querySelector('.cat-info'),
+};
+
+refs.error.classList.add('hidden');
+
+fetchBreeds()
+  .then(breeds => {
+    refs.breedSelect.classList.remove('hidden');
+    refs.loader.classList.add('hidden');
+
+    breeds.forEach(breed => {
+      const option = document.createElement('option');
+      option.value = breed.id;
+      option.textContent = breed.name;
+      refs.breedSelect.appendChild(option);
+    });
+    new SlimSelect({
+      select: '.breed-select',
+    });
+  })
+  .catch(error => {
+    Notiflix.Notify.failure(
+      'Oops! Something went wrong! Try reloading the page!'
+    );
+  });
+
+refs.breedSelect.addEventListener('change', function () {
+  const selectedBreedId = refs.breedSelect.value;
+
+  refs.catInfo.classList.add('hidden');
+  refs.loader.classList.remove('hidden');
+  fetchCatDetails(selectedBreedId);
+});
+
+function fetchCatDetails(breedId) {
+  fetchCatByBreed(breedId)
+    .then(showCatInfo)
+    .catch(error => {
+      Notiflix.Notify.failure(
+        'Oops! Something went wrong! Try reloading the page!'
+      );
+    });
+}
+function showCatInfo(cat) {
+  refs.catInfo.classList.remove('hidden');
+  refs.loader.classList.add('hidden');
+  const { name, description, temperament } = cat.breeds[0];
+
+  refs.catInfo.innerHTML = `
+    <img src="${cat.url}" alt="Cat Image" width="500" ;class="cat-image">
+    <div class="cat-container-info">
+      <h2 class="cat-name">${name}</h2>
+      <p class="cat-description">${description}</p>
+      <p class="cat-temperament"><strong>Temperament:</strong> ${temperament}</p>
+    </div>`;
+}
